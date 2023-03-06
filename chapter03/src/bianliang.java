2f4dc4549d5d56ae65abbe4b900007c3afb77ff6
JinW1228 <76034195+JinW1228@users.noreply.github.com>
2023-03-06 14:58:22 +0800

    first add files

---
+++ chapter03/src/bianliang.java
@@ -0,0 +1,25 @@
+public class bianliang {
+
+    //编写一个main方法
+    public static void main(String[] args) {
+
+        char c1 = 97;
+        System.out.println(c1);
+
+        char c2 = 'a';
+        System.out.println((int)c2);
+
+        char c3 = '吴';
+        System.out.println((int)c3);
+        System.out.println(c3);
+
+        char c4 = '3';
+        System.out.println(c4);
+
+        System.out.println('a');
+        System.out.println('a' + 10);
+
+
+    }
+
+}
